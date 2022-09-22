aff7cef5a4b211ce3716e42544dd5bd205a7095b
Ben Brennan <benbrennan@Bens-MacBook-Pro-5.local>
2022-09-22 19:01:38 -0400

    I think these work but are pretty pointless, would be good to go back in and understand the backend of this code tho

--- turing_horseshoe.jl
+++ turing_horseshoe.jl
@@ -1,10 +1,18 @@
 using Turing 
 using LinearAlgebra
 using FillArrays 
+using ReverseDiff
+using Memoization
+
 
 struct HP{T}
     X::T
 end 
+
+Turing.setadbackend(:reversediff)
+Turing.setrdcache(true)
+
+
 
 @model function(m::HP)(::Val{:Ungrouped})
     X = m.X 
@@ -13,40 +21,51 @@ end
     τ ~ halfcauchy
     λ ~ filldist(halfcauchy, q)
     σ ~ Exponential(1)
-    β ~ MvNormal(zeros(q), diagm(λ .* (τ*σ)).^2)
+    β ~ MvNormal(zeros(q), Diagonal((λ .* (τ*σ).^2)))
 
     y ~ MvNormal(X*β, σ^2 * I)
 
     return(; τ , λ, β, σ, y)
 end 
 
-@model function(m::HP)(::Val{Grouped}, group = NaN ) # TODO: How do I assign this group? 
+@model function(m::HP)(::Val{:Grouped}, group = NaN) # TODO: How do I assign this group? 
 
     X = m.X 
     q = size(X,2)
-    g = size(group)
+    X = Matrix(X)
+    group = Matrix(group)
+    g = size(group)[1]
     g_sizes = [sum(group .== i) for i in 1:maximum(group)]; 
     halfcauchy = truncated(Cauchy(0,1); lower = 0)
     τ ~ halfcauchy
     λ ~ filldist(halfcauchy, q)
     γ ~ filldist(halfcauchy, g)
+    σ ~ Exponential(1)
+    β  = Vector{Float64}(undef, q)
     k = 1 
     lam = 1
     gam = 1
     for size in g_sizes  
         m = size + (k-1);
         for j in k:m
-            β ~ (0,λ[lam]*γ[gam]*τ*σ)
+            β[lam] ~ Normal(0,(λ[lam]*γ[gam]*τ*σ)^2)
             lam +=1 
         end 
-        gma += 1 
+        gam += 1 
         k += size 
+    end
     y ~ MvNormal(X*β, σ^2 * I)
 end 
 
 
-model = HP(X)(Val(:Ungrouped)); 
+model_un = HP(X)(Val(:Ungrouped)); 
+
+model_y_un = model_un| (; y)
+
+samps_un = sample(model_y, NUTS(), MCMCThreads(), 2_000, 2)
+
+model = HP(X)(Val(:Grouped), g); 
 
 model_y = model | (; y)
 
-samps = sample(model_y, NUTS(), MCMCThreads(), 2_000, 4)
+samps = sample(model_y, NUTS(), MCMCThreads(), 2_000, 2)
